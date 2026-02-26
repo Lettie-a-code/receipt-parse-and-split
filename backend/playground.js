58d4609db9442622691e368f766e2e57817ec588
aviedu5 <150853378+aviedu5@users.noreply.github.com>
2026-02-26 12:41:18 -0800

    Add files via upload

---
+++ backend/playground.js
@@ -0,0 +1,7 @@
+const { equalSplit } = require("./splitEngine");
+
+const people = ["Eduardo", "Alex", "Maria"];
+const total = 90.35
+const result = equalSplit(total, people);
+
+console.log(result);
